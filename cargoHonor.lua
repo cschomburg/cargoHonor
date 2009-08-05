7df3e30f6fdead3ebc7fe7fd872ed60e237af378
Constantin <xconstruct@gmail.com>
2009-08-05 15:19:33 +0200

    rewrite for 3.2, semi-inclusion of IoC

--- cargoHonor.lua
+++ cargoHonor.lua
@@ -1,20 +1,68 @@
 -- Short configuration
-local hideMarks = nil	--[[	true: hides Marks of Honor tooltip information			 ]]
-local hideWinLoss = nil	--[[	 true: hides Win/Loss statistics on tooltip				 ]]
-local playerFaction = UnitFactionGroup("player")
+local hideMarks = false			--[[	true: hides Marks of Honor tooltip information		]]
+local hideWinLoss = false		--[[	true: hides Win/Loss statistics on tooltip			]]
+local hideWintergrasp = false	--[[	true: hides the wait time for Wintergrasp			]]
 
-local BGs = { "Alterac Valley", "Warsong Gulch", "Strand of the Ancients", "Arathi Basin", "Eye of the Storm",	-- Long form
-			"Alterac", "Warsong", "SotA", "Arathi", "EotS" }	-- Short form
-local achIDs = { 53, 52, 1549, 55, 54,	-- Total
-				49, 105, 1550, 51, 50 }	-- Won
-local itemIDs = { 20560, 20558, 42425, 20559, 29024 }
+local info = {
+	{
+		name = "Alterac Valley",
+		abbr = "Alterac",
+		achTotal = 53,
+		achWon = 49,
+		itemID = 20560,
+	},{
+		name = "Arathi Basin",
+		abbr = "Arathi",
+		achTotal = 55,
+		achWon = 51,
+		itemID = 20559,
+	},{
+		name = "Eye of the Storm",
+		abbr = "EotS",
+		achTotal = 54,
+		achWon = 50,
+		itemID = 29024,
+	},{
+		name = "Isle of Conquest",
+		abbr = "IoC",
+	}{
+		name = "Strand of the Ancients",
+		abbr = "SotA",
+		achTotal = 1549,
+		achWon = 1550,
+		itemID = 42425,
+	},{
+		name = "Warsong Gulch",
+		abbr = "Warsong",
+		achTotal = 52,
+		achWon = 105,
+		itemID = 20558,
+	},
+	{
+		name = "Wintergrasp",
+		abbr = "WG",
+		itemID = 43589,
+	},
+}
 
--- Get the suffix for the selected display
-local getSuffix = function(i)
-	return (i == 5 and "honor") or (i == 4 and " arena") or (i == 3 and " bg") or (i == 2 and " today") or " total"
-end
+local suffixes = {
+	"total",
+	"today",
+	"bg",
+	"arena",
+	"honor",
+}
+
+local holidays = {
+	"Alterac",
+	"Warsong",
+	"SotA",
+	"Arathi",
+	"EotS",
+}
 
 -- Initializing the object and frame
+local playerFaction = UnitFactionGroup("player")
 local OnEvent = function(self, event, ...) self[event](self, event, ...) end
 local dataobj = LibStub:GetLibrary("LibDataBroker-1.1"):NewDataObject("cargoHonor", {
 	type = "data source",
@@ -25,13 +73,15 @@ local dataobj = LibStub:GetLibrary("LibDataBroker-1.1"):NewDataObject("cargoHono
 })
 local frame = CreateFrame"Frame"
 
--- Print the next bg weekend and if it's currently active
-local function GetCurrentNextWeekend()
+-- Print the next bg holiday and if it's currently active
+local function GetBattlegroundHoliday()
+	local max = #holidays
 	local now = date("*t")
 	local week = floor(now.yday/7)+1
 	if(now.wday == 3) then week = week +1 end
-	week = (week + 2) % 5
-	return week > 0 and week or 5, now.wday > 5 or now.wday < 3
+	week = (week + 2) % max
+	week = week > 0 and week or max
+	return holidays[week], now.wday > 5 or now.wday < 3
 end
 
 -- Color function for Marks of Honor
@@ -44,50 +94,55 @@ local function ColorGradient(perc, r1, g1, b1, r2, g2, b2, r3, g3, b3)
 end
 
 -- Get percent, win total info by battleground id
-local function GetWinTotal(id)
+local function GetWinTotal(bg)
 	local total, won
 	if(not id) then
 		total, won = GetStatistic(839), GetStatistic(840)
 	else
-		total, won = GetStatistic(achIDs[id]), GetStatistic(achIDs[id+5])
+		total, won = GetStatistic(info[name].achTotal), GetStatistic(info[name].achWon)
 	end
-	if(total == "--") then total = 0 else total = tonumber(total) end
+	if(total == "--") then total = 0 else total = tonumber(total) or 0 end
 	if(won == "--") then won = 0 else won = tonumber(won) end
 	
-	return total > 0 and won/total, won, total
+	return won, total
 end
 
 -- [[    Update the display !    ]] --
 local session, total, startTime
 local startHonor, isBG
 function frame:HONOR_CURRENCY_UPDATE()
-	if(cargoHonor.displ == 5) then
-		local string = ""
+	local displ = cargoHonor.displ
+	local value
+
+	local session = select(2, GetPVPSessionStats())
+
+	if(displ == 5) then
+		value = ""
 		if(isBG) then
-			string = string..(select(2, GetPVPSessionStats()) - startHonor or 0).. " | "
+			value = value..(session - startHonor or 0).. " | "
 		end
-		string = string..select(2, GetPVPSessionStats()).." | "..GetHonorCurrency().." "
-		dataobj.value = string
-	elseif(cargoHonor.displ == 4) then
-		dataobj.value =  GetArenaCurrency()
-	elseif(cargoHonor.displ == 3) then
+		value = value..session.." | "..GetHonorCurrency()
+	elseif(displ == 4) then
+		value =  GetArenaCurrency()
+	elseif(displ == 3) then
 		if(startHonor) then
-			dataobj.value = select(2, GetPVPSessionStats()) - startHonor or 0
+			value = session - startHonor or 0
 		else
-			dataobj.value = 0
+			value = 0
 		end
-	elseif(cargoHonor.displ == 2) then
-		dataobj.value = select(2, GetPVPSessionStats())
+	elseif(displ == 2) then
+		value = session
 	else
-		dataobj.value = GetHonorCurrency()
+		value = GetHonorCurrency()
 	end
-	dataobj.text = dataobj.value.." "..dataobj.suffix
+	databroker.value = value
+	dataobj.text = value.." "..dataobj.suffix
 end
 
 --[[   Initialize all variables    ]] --
 function frame:PLAYER_ENTERING_WORLD()
 	if(not cargoHonor) then cargoHonor = {} end
-	session = select(2, GetPVPSessionStats())
+	local session = select(2, GetPVPSessionStats())
 	if(startHonor and isBG) then
 		cargoHonor.LastBG = session - startHonor
 	end
@@ -107,52 +162,62 @@ frame:RegisterEvent"PLAYER_ENTERING_WORLD"
 
 -- [[   The tooltip  ]] --
 function dataobj.OnTooltipShow(tooltip)
-	local id, isWeek = GetCurrentNextWeekend()
+
+	-- Honor Stats
+	local bgHoliday, isHoliday = GetBattlegroundHoliday()
 	session = select(2, GetPVPSessionStats())
 	total = GetHonorCurrency()
 	local perHour
 	if(startTime) then perHour = (session-startSession)/((time()-startTime)/3600) end
 	
-	tooltip:AddDoubleLine(total.." Honor", isWeek and BGs[id+5], 0,1,0, 0,1,0)
-	tooltip:AddDoubleLine("Today:", string.format("|cff00ff00%i|r (|cff00ff00%i|r/h)", session, perHour or 0), 1,1,1, 1,1,1)
+	tooltip:AddDoubleLine(total.." Honor", isHoliday and bgHoliday, 0,1,0, 0,1,0)
+	tooltip:AddDoubleLine("Today:", ("|cff00ff00%i|r (|cff00ff00%i|r/h)"):format(session, perHour or 0), 1,1,1, 1,1,1)
 	if(isBG and startHonor) then
 		tooltip:AddDoubleLine("This BG:", session-startHonor, 1,1,1, 0,1,0)
 	end
 	if(cargoHonor and cargoHonor.LastBG) then
 		tooltip:AddDoubleLine("Last BG:", cargoHonor.LastBG, 1,1,1, 0,1,0)
 	end
-	tooltip:AddDoubleLine("Arena points:", GetArenaCurrency(), 1,1,1, 0,1,0)
-	if(isWeek) then
-		id = id+1
-		id = (id == 6) and 1 or id
-	end
-	tooltip:AddDoubleLine("Next weekend:", BGs[id+5], 1,1,1, 1,1,1)
 
+	-- Arena points
+	tooltip:AddDoubleLine("Arena points:", GetArenaCurrency(), 1,1,1, 0,1,0)
+
+	-- Next holiday
+	if(not isHoliday) then
+		tooltip:AddDoubleLine("Next holiday:", bgHoliday, 1,1,1, 1,1,1)
+	end
+
+	-- Marks
 	if(not hideMarks) then
 		tooltip:AddLine(" ")
 		tooltip:AddLine("Marks of Honor")
-		local marks
-		for i=1, 5 do
-			marks = GetItemCount(itemIDs[i], true)
+		for i, bg in ipairs(info) do
+			local marks = GetItemCount(bg.itemID, true)
 			if(marks > 0) then
-				tooltip:AddDoubleLine(BGs[i], marks, 1,1,1, ColorGradient(marks/100, 1,0,0, 1,1,0, 0,1,0))
+				tooltip:AddDoubleLine(bg.name, marks, 1,1,1, ColorGradient(marks/100, 1,0,0, 1,1,0, 0,1,0))
 			end
 		end
 	end
-	local percent, win, total = GetWinTotal()
+
+	-- Win/Loss Ratio
 	if(not hideWinLoss and percent) then
 		tooltip:AddLine(" ")
 		tooltip:AddLine("Win/Loss Ratio")
-		for i=1, 5 do
-			percent, win, total = GetWinTotal(i)
-			if(percent) then
+		for i, bg in ipairs(info) do
+			local win, total = GetWinTotal(i)
+			if(total > 0) then
 				tooltip:AddDoubleLine(
-					format("%dx %s:", total, BGs[i]),
-					format("%.0f|cffffffff%%|r", percent*100),
-					1,1,1, ColorGradient(percent, 1,0,0, 1,1,0, 0,1,0)
+					("%dx %s:"):format(total, bg.name),
+					("%.0f|cffffffff%%|r"):format(win/total*100),
+					1,1,1, ColorGradient(win/total, 1,0,0, 1,1,0, 0,1,0)
 				)
 			end
 		end
+	end
+
+	if(not hideWintergrasp) then
+		local wgTime = SecondsToTimeAbbrev(GetWintergraspWaitTime())
+		tooltip:AddLine("Wintergrasp start:", wgTime, 1,1,1, 0,1,0)
 	end
 	tooltip:AddLine(" ")
 	tooltip:AddLine("Click to toggle display")
@@ -161,15 +226,16 @@ end
 
 function dataobj.OnClick(self, button)
 	if(button == "RightButton") then
-		ToggleFrame(PVPParentFrame)
+		TogglePVPFrame()
 	else
 		cargoHonor.displ = (cargoHonor.displ == 5 and 1) or (cargoHonor.displ and cargoHonor.displ+1) or 2
-		if(cargoHonor.displ == 4) then
+		local displ = cargoHonor.displ
+		if(displ == 4) then
 			dataobj.icon = [[Interface\PVPFrame\PVP-ArenaPoints-Icon]]
-		elseif(cargoHonor.displ == 1) then
+		elseif(displ == 1 or displ == 5) then
 			dataobj.icon = "Interface\\AddOns\\cargoHonor\\"..playerFaction.."Icon"
 		end
-		dataobj.suffix = getSuffix(cargoHonor.displ)
+		dataobj.suffix = suffixes[displ]
 		frame:HONOR_CURRENCY_UPDATE()
 	end
 end
